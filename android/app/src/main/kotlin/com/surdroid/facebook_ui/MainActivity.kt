2f502c949fd3ab77be263db336769aa3a5db0076
Surhid Amatya <surhid.amatya@gmail.com>
2020-11-08 20:05:23 +0545

    main: first commit

---
+++ android/app/src/main/kotlin/com/surdroid/facebook_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.surdroid.facebook_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:facebook_ui/config/palette.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Fb UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold
+      ),
+      home: NavScreen(),
+    );
+  }
+}
